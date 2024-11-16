9f2699405b8a62074217fd9c02fef8df5bb3d768
Osa-nag00 <naghiseosa2000@gmail.com>
2024-11-16 11:43:20 -0500

    made the run script more verbose

--- run_script.sh
+++ run_script.sh
@@ -1,17 +1,47 @@
 #!/bin/bash
 
-# TODO: maybe come back and make this script more verbose?
+# VARIABLES
+song_dl_dir=''
+youtube_link=''
 
-if [ $# -ne 2 ]; then
-    echo "Incorrect number of arguments"
+# FUNCTIONS
+# Function to display script usage
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo " -h       Display this help message"
+    echo -e "\nRequired:"
+    echo " -l       Used to specify the youtube link to download"
+    echo " -d       Used to specify to dir of where to download the mp3"
+}
+
+# if there is ":" after letter, it means that opt is required
+# OPTARG is the current flag being read
+while getopts "hl:d:" flag; do
+    case $flag in
+    h)
+        usage
+        ;;
+    l)
+        youtube_link=$OPTARG
+        ;;
+    d)
+        song_dl_dir=$OPTARG
+        ;;
+
+    \?)
+        usage
+        ;;
+    esac
+done
+
+if [[ -z $youtube_link ]] || [[ -z $song_dl_dir ]]; then
+    echo -e "You must specify both -l and -d\n"
+    usage
     exit 1
 fi
 
-# variables used to pass the link on
-song_dl_dir=$1
-youtube_link=$2
+echo -e "\nRunning Python script"
+python "ytos_script.py" "$youtube_link" "$song_dl_dir"
 
-python "ytos_script.py" $song_dl_dir $youtube_link
-
-# after the python script has finish running
 exit 0
