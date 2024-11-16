29fd29666c45970e333c80e92ce7337de647edbd
Osa-nag00 <naghiseosa2000@gmail.com>
2024-11-16 10:39:09 -0500

    added number of arguments check in run script, updated requirements.txt

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-echo "Running Python script to download mp3"
+# TODO: maybe come back and make this script more verbose?
 
+if [ $# -ne 2 ]; then
+    echo "Incorrect number of arguments"
+    exit 1
+fi
+
+# variables used to pass the link on
+song_dl_dir=$1
+youtube_link=$2
+
+python "ytos_script.py" $song_dl_dir $youtube_link
+
+# after the python script has finish running
+exit 0
