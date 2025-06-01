c3808124cb09d9dfdd40e0cfd347ae9df57d2325
Osa-nag00 <naghiseosa2000@gmail.com>
2025-06-01 15:05:59 -0400

    rename runner file to better name

--- ytos.py
+++ ytos.py
@@ -1,11 +1,12 @@
 import sys
-from rich.console import Console
-import click
 
-import src.validator as v
+import click
 import src.downloadUtil as du
-import src.printer as printer
 import src.fileManip as fm
+import src.printer as printer
+import src.validator as v
+from rich.console import Console
+
 
 @click.command()
 @click.option(
@@ -17,11 +18,14 @@ import src.fileManip as fm
     help="path to txt file of youtube links separated by new lines, cannot be used it --link",
 )
 @click.option(
-    "-d", "--save-dir", "save_dir", required=True, type=click.STRING, help="Path to where the mp3 will be downloaded"
+    "-d",
+    "--save-dir",
+    "save_dir",
+    required=True,
+    type=click.STRING,
+    help="Path to where the mp3 will be downloaded",
 )
-
 def ytos(save_dir: str, filePath: str) -> None:
-    
     """
     Youtube to spotify script will take a given a text file of youtube links to be downloaded to mp3
     of the specified video to a Spotify local folder
@@ -30,20 +34,27 @@ def ytos(save_dir: str, filePath: str) -> None:
     console = Console()
     errConsole = Console(stderr=True)
 
-
-    entryMsg: str = f"Attempting to download songs from list [bold green]{printer.linkString(filePath, filePath)}[/bold green] to [italic green]{printer.linkString(save_dir,save_dir)}[/italic green]"
+    entryMsg: str = (
+        f"Attempting to download songs from list [bold green]{printer.linkString(filePath, filePath)}[/bold green] to [italic green]{printer.linkString(save_dir,save_dir)}[/italic green]"
+    )
     console.print(entryMsg + "\n")
 
     if not v.is_download_dir_valid(save_dir):
-        errConsole.print(f"Download directory: [bold red]{save_dir}[/bold red] is not a valid directory to download the mp3 to")
+        errConsole.print(
+            f"Download directory: [bold red]{save_dir}[/bold red] is not a valid directory to download the mp3 to"
+        )
         sys.exit(-1)
     elif not v.file_exists(filePath):
-        errConsole.print(f"List of youtube links : [bold red]{filePath}[/bold red] is not a valid file")
+        errConsole.print(
+            f"List of youtube links : [bold red]{filePath}[/bold red] is not a valid file"
+        )
         sys.exit(1)
     else:
         links: list[str] = fm.readLinksFromFile(filePath)
         for link in links:
-            console.print(f"\nWorking on [bold green]{printer.linkString(link, link)}[/bold green]")
+            console.print(
+                f"\nWorking on [bold green]{printer.linkString(link, link)}[/bold green]n"
+            )
             du.download_mp3_to_dir(link, save_dir)
         console.print("Done")
 
